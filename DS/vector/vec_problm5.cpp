10aa32d705459967d2963ff3518cf6dbe899196a
Ziad Walid <ziyadskillup@gmail.com>
2026-02-13 12:04:25 +0200

    vec_problm5

---
+++ DS/vector/vec_problm5.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+using namespace std;
+void assert(bool condition, const char *message)
+{
+    if (!condition)
+    {
+        cout << "Assertion failed: " << message << endl;
+        exit(1);
+    }
+}
+
+class Vector
+{
+private:
+    int *arr{nullptr};
+    int size{0};
+    int capacity{};
+        void expand_capacity()
+    {
+        capacity *= 2;
+        cout << "expand capacity to " << capacity << "\n";
+        int *arr2 = new int[capacity]{};
+        for (int i = 0; i < size; ++i)
+        {
+            arr2[i] = arr[i];
+        }
+        swap(arr, arr2);
+        delete[] arr2;
+    }
+
+public:
+    Vector(int size) : size(size)
+    {
+
+        if (size < 0)
+            size = 1;
+        capacity = size + 10;
+        arr = new int[capacity]{};
+    }
+
+
+    ~Vector()
+    {
+        delete[] arr;
+        arr = nullptr;
+    }
+
+    void set(int idx, int val)
+    {
+        assert(0 <= idx && idx < size, "Index out of range in set()");
+        arr[idx] = val;
+    }
+
+    void print()
+    {
+        cout << "index: ";
+        for (int i = 0; i < size; ++i){
+            cout << i << " | ";
+        }
+        cout << "\n" << "value: ";
+        for (int i = 0; i < size; ++i)
+            cout << arr[i] << " | ";
+        cout << "\n";
+    }
+    int find_transposition (int value){
+        --value;
+        for (int i = 0; i < size; ++i){
+            if (value == i){
+                if (i == 0){
+                return 1;
+            }
+                swap (arr[i], arr[i-1]);
+                print();
+                return i -1;
+            }
+        }
+        return -1;
+    }
+};
+int main()
+{
+    Vector v(6);
+    for (int i = 0; i < 6; ++i){
+        v.set(i,i+1);
+    }
+    v.print();
+    v.find_transposition(3);
+    return 0;
+}
